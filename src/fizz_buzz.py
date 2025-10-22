2638dc4cdb3603c849a0735fdb60384fe75a1dc3
fronre <islamhala35@gmail.com>
2025-10-22 20:44:46 +0100

    test for fizz_buzz with input 3.

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -4,7 +4,7 @@ def fizz_buzz(number: int) -> str:
    if number == 2 :
      return '2'
    if number == 3 :
-       return '3'
+       return 'buzz'
    if number == 4 :
        return '4'
    return 'fizz buzz'
