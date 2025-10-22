9b36298364c93d06e62d5549b450fa5ad41ff7f0
fronre <islamhala35@gmail.com>
2025-10-22 21:25:37 +0100

    refactoring

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,5 +1,7 @@
 def fizz_buzz(number: int) -> str:
-    if number % 3 == 0 :
-      return 'fizz'
-    if number % 5 == 0 :
-      return 'buzz'
+    if number % 3 == 0 and number % 5 == 0:
+        return 'fizzbuzz'
+    if number % 3 == 0:
+        return 'fizz'
+    if number % 5 == 0:
+        return 'buzz'
