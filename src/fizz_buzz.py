ede6a458bc44e161e12087d3256e1e03332ff84a
fronre <islamhala35@gmail.com>
2025-10-22 21:23:55 +0100

    refactoring

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,27 +1,5 @@
 def fizz_buzz(number: int) -> str:
-   if number == 1 :
-      return '1'
-   if number == 2 :
-     return '2'
-   if number == 3 :
-       return 'fuzz'
-   if number == 4 :
-       return '4'
-   if number == 5 :
-       return 'buzz'
-   if number == 6 :
-       return 'fizz'
-   if number == 10 :
-       return 'buzz'
-   if number == 11 :
-       return '11'
-   if number == 12 :
-       return 'fizz'
-   if number == 13 :
-       return '13'
-   if number == 15 :
-       return 'fizzbuzz'
-   if number == 18 :
-       return 'fizzbuzz'
-
-   return 'fizz buzz'
+    if number % 3 == 0 :
+      return 'fizz'
+    if number % 5 == 0 :
+      return 'buzz'
