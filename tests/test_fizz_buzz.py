998abcf2f6a8648c12a47cb045c1883c4ec7eb2e
fronre <islamhala35@gmail.com>
2025-10-22 20:33:10 +0100

    test for fizz_buzz with input 2

--- tests/test_fizz_buzz.py
+++ tests/test_fizz_buzz.py
@@ -1,5 +1,12 @@
+import pytest
 from src.fizz_buzz import fizz_buzz
 
-def test_number_one_returns_fizz_buzz():
-    assert fizz_buzz(1) == '1'
+@pytest.mark.parametrize('number, result_expected', [
+    (0, 'fizzbuzz'),
+    (1, '1'),
+    (2, '2')
+])
+def test_number_returns_result_expected(number, result_expected):
+    actual_result = fizz_buzz(number)
+    assert actual_result == result_expected
 
